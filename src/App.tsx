a07a321f9c54bf47d00a20c1553a6f71ece0caad
AntonioToni <antonio.gaming10@gmail.com>
2022-11-29 01:45:46 +0100

    Design of the site (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Fab from '@mui/material/Fab';
+import LocationSearchingIcon from '@mui/icons-material/LocationSearching';
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  function getLocation() {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      console.log("Latitude is : ", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+      localStorage.setItem("Latitude", JSON.stringify(position.coords.latitude));
+      localStorage.setItem("Longitude", JSON.stringify(position.coords.longitude));
+    });
+  }
+  
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <div className='alignCenter'>
+        <Fab className='locateButton' onClick={getLocation}>
+          <LocationSearchingIcon/>
+        </Fab>
+        <div className='currentWeather'>
+          <h1>City</h1>
+          <h1>10°C</h1>
+          <h2>Mostly Cloudy</h2>
+          <h2>H:13°C L:4°C</h2>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </>
   )
 }
 
