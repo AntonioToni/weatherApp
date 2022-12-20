7008c6b0b37302459a3745cf07bca127dce13ad7
Antonio <antonio.gaming10@gmail.com>
2022-12-20 17:44:11 +0100

    Updated code to work with interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,45 @@
 import './App.css'
 import LocationSearchingIcon from '@mui/icons-material/LocationSearching';
 import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Weather } from './classes/Weather';
-import env from './env.json';
 import { BasicWeather } from './components/basicWeather';
 import { DetailedWeather } from './components/detailedWeather';
 import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
+import { getIconUrl, searchLocation } from "./services/weatherService";
+import { readWeather } from './services/weatherService';
+import { readForecast } from './services/weatherService';
+import { Weather } from './model/Weather';
 
 function App() {
-  const [data, setData] = useState(new Weather);
   const [city, setCity] = useState('');
   const [error, setError] = useState('');
+  const [weather, setWeather] = useState<Weather | null>(null);
+  const [forecast, setForecast] = useState<Weather[] | null>(null);
   let latitude = parseFloat(JSON.parse(localStorage.getItem("Latitude") || '0'));
   let longitude = parseFloat(JSON.parse(localStorage.getItem("Longitude") || '0'));
+
+  if (localStorage.getItem("Latitude") === null || localStorage.getItem("Longitude") === null) {
+    getCurrentPosition();
+  }
+
+  useEffect(() => {
+    (async function () {
+      getWeatherData(latitude, longitude);
+    })()
+  }, []);
+
+  async function getWeatherData(latitude: number, longitude: number){
+    const [weather, forecast] = await Promise.all([
+      readWeather(latitude, longitude),
+      readForecast(latitude, longitude)
+    ]);
+    setWeather(weather);
+    setForecast(forecast);
+    console.log(weather);
+    console.log(forecast);
+  }
 
   function setLatLon(lat: number, lon: number){
     localStorage.setItem("Latitude", JSON.stringify(lat));
@@ -25,63 +48,34 @@ function App() {
     longitude = lon;
   }
 
-  function getLocation() {
+  let getCityLocation = async (term: string) => {
+    const location = await searchLocation(term);
+    if (!location[0]) {
+      setError(`No location found called '${term}'`);
+    } else {
+      setLatLon(location[0].lat, location[0].lon);
+      getWeatherData(latitude, longitude);
+    }
+  };
+
+  function getCurrentPosition() {
     navigator.geolocation.getCurrentPosition(function(position) {
       console.log("Latitude is : ", position.coords.latitude);
       console.log("Longitude is :", position.coords.longitude);
       setLatLon(position.coords.latitude, position.coords.longitude);
-      getWeatherInfo();
+      getWeatherData(latitude, longitude);
     });
-  }
-
-  if (localStorage.getItem("Latitude") === null || localStorage.getItem("Longitude") === null) {
-    getLocation();
-  }
-
-  const getWeatherInfo = () => {
-    let url= `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&id=524901&appid=${env.apiKey}&units=metric`
-    console.log(url);
-    axios.get(url).then((response) => {
-      setData(response.data)
-      console.log(response.data)
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-  }
-
-  useEffect(() => {
-    console.log("Loading data...");
-    getWeatherInfo();
-  }, []);
-
-  const getCityPosition = () => {
-    let url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${env.apiKey}`
-    console.log(url);
-    axios.get(url).then((response) => {
-      if (response.data[0] === undefined) {
-        setError("Invalid location")
-      }
-      else{
-        setError("");
-        console.log(response.data[0].lat, response.data[0].lon);
-        setLatLon(response.data[0].lat, response.data[0].lon);
-        getWeatherInfo();
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-    })
   }
 
   const handleSubmit = (event: any) => {
     event.preventDefault();
-    console.log(city);
-    getCityPosition();
+    getCityLocation(city);
   }
   
   const favicon = document.getElementById("favicon") as HTMLAnchorElement
-  favicon!.href = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+  if (weather) {
+    favicon!.href = getIconUrl(weather?.weather[0].icon);
+  }
 
   return (
     <>
@@ -91,7 +85,7 @@ function App() {
           onSubmit={handleSubmit}
           sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 250 }}
           >
-          <IconButton onClick={getLocation} aria-label="menu">
+          <IconButton onClick={getCurrentPosition} aria-label="menu">
             <LocationSearchingIcon/>
           </IconButton>
           <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
@@ -105,8 +99,8 @@ function App() {
         {
           error ? <div className='error'>{error}</div> : null
         }
-        <BasicWeather data = {data}/>
-        <DetailedWeather data = {data}/>
+        <BasicWeather data = {weather}/>
+        <DetailedWeather data = {weather}/>
       </div>
     </>
   )
