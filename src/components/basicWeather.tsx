d468d05815a34b314ca16f62c5d52317a18bd946
Antonio <antonio.gaming10@gmail.com>
2023-01-27 04:06:44 +0100

    basicWeather info -> MUI

--- src/components/basicWeather.tsx
+++ src/components/basicWeather.tsx
@@ -1,6 +1,7 @@
+import { Stack } from '@mui/system';
 import react from 'react';
 import { Weather } from '../model/Weather';
-
+import { Typography } from '@mui/material';
 export function BasicWeather(props: {
   data : Weather | null}
   ) {
@@ -8,11 +9,11 @@ export function BasicWeather(props: {
     return null;
   };
   return(
-    <div className='currentWeather'>
-      <h1>{props.data.name}</h1>
-      <h1>{Math.round(props.data?.main.temp)}°C</h1>
-      <h2>{props.data.weather[0].description.charAt(0).toUpperCase() + props.data.weather[0].description.slice(1)}</h2>
-      <h2>H:{Math.round(props.data.main.temp)}°C L:{Math.round(props.data.main.temp_min)}°C</h2>
-    </div>
+    <Stack spacing={2} alignItems='center' sx={{marginBottom: 2}}>
+      <Typography variant='h2'>{props.data.name}</Typography>
+      <Typography variant='h2'>{Math.round(props.data?.main.temp)}°C</Typography>
+      <Typography variant='h4'>{props.data.weather[0].description.charAt(0).toUpperCase() + props.data.weather[0].description.slice(1)}</Typography>
+      <Typography variant='h5'>H:{Math.round(props.data.main.temp)}°C L:{Math.round(props.data.main.temp_min)}°C</Typography>
+    </Stack>
   )
 }
