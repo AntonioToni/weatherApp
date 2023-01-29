6937475dc89dc99df6be8728a52b5aa2879fe220
Antonio <antonio.gaming10@gmail.com>
2023-01-29 02:58:04 +0100

    weatherForecast completely switched to MUI

--- src/components/weatherForecast.tsx
+++ src/components/weatherForecast.tsx
@@ -1,18 +1,28 @@
 import react from "react";
 import { Weather } from "../model/Weather";
 import { WeatherEntry } from "./weatherEntry";
-import './weatherForecast.css';
-import { Stack, Typography} from '@mui/material'
+import { Stack, Typography, Box} from '@mui/material'
 
 export function WeatherForecast(props : {
   forecast : Weather[] | null;
 }) {
+
   if (!props.forecast) {
     return null;
   }
+
   return(
     <>
-      <div className="forecastContainer">
+      <Box sx={{
+        backgroundColor: 'rgba(0, 0, 0, 0.2)',
+        padding: '11px',
+        borderRadius: '10px',
+        margin: '0 0 10px 0',
+        backdropFilter: 'blur(6px)',
+        '@media (max-width: 600px)' : {
+          width: '85vw'
+        }
+      }}>
         <Typography>Forecast</Typography>        
         <Stack direction='row' sx={{overflow: 'auto', whiteSpace: 'nowrap'}}>
           {props.forecast.map(timePoint =>
@@ -21,7 +31,7 @@ export function WeatherForecast(props : {
             </Stack>
             )}
         </Stack>
-      </div>
+      </Box>
     </>
   )
 }
