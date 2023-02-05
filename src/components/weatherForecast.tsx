b7b1bd34ef7008ac9d437389bfd6aece1121feda
Antonio <antonio.gaming10@gmail.com>
2023-02-05 15:34:21 +0100

    sx for forecast separated to const

--- src/components/weatherForecast.tsx
+++ src/components/weatherForecast.tsx
@@ -11,18 +11,20 @@ export function WeatherForecast(props : {
     return null;
   }
 
+  const forecastContainer = {
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    padding: '11px',
+    borderRadius: '10px',
+    margin: '0 0 10px 0',
+    backdropFilter: 'blur(6px)',
+    '@media (max-width: 600px)' : {
+      width: '85vw'
+    }
+  }
+
   return(
     <>
-      <Box sx={{
-        backgroundColor: 'rgba(0, 0, 0, 0.2)',
-        padding: '11px',
-        borderRadius: '10px',
-        margin: '0 0 10px 0',
-        backdropFilter: 'blur(6px)',
-        '@media (max-width: 600px)' : {
-          width: '85vw'
-        }
-      }}>
+      <Box sx={forecastContainer}>
         <Typography>Forecast</Typography>        
         <Stack direction='row' sx={{overflow: 'auto', whiteSpace: 'nowrap'}}>
           {props.forecast.map(timePoint =>
